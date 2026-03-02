2b0d57e9e711bad28f973e73098bb1ba5e990df9
josevegara04 <jbvegar@eafit.edu.co>
2026-03-01 20:56:45 -0500

    <feat>: add image views

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render, redirect
-from django.views.generic import TemplateView
+from multiprocessing import Value
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import TemplateView, ListView
 from django.views import View
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django import forms
+from .models import Product
+from .utils import ImageLocalStorage
 
 class HomePageView(TemplateView):
     template_name = "pages/home.html"
@@ -20,13 +25,7 @@ class AboutPageView(TemplateView):
         
         return context
     
-class Product:
-    products = [
-        {"id": "1", "name": "TV", "description": "Best TV"},
-        {"id": "2", "name": "IPhone", "description": "Best IPhone"},
-        {"id": "3", "name": "Chromecast", "description": "Best Chromecast"},
-        {"id": "4", "name": "Glasses", "description": "Best Glasses"}
-    ]
+
     
 class ProductIndexView(View):
     template_name = 'products/index.html'
@@ -35,7 +34,7 @@ class ProductIndexView(View):
         viewData = {}
         viewData["title"] = "Products - Online Store"
         viewData["subtitle"] = "List of products"
-        viewData["products"] = Product.products
+        viewData["products"] = Product.objects.all()
         
         return render(request, self.template_name, viewData)
     
@@ -43,22 +42,45 @@ class ProductShowView(View):
     template_view = "products/show.html"
     
     def get(self, request, id):
+        try:
+            product_id = int(id)
+            if product_id < 1:
+                raise ValueError("Product id must ve 1 or greater")
+            product = get_object_or_404(Product, pk=product_id)
+        except (ValueError, IndexError):
+            return HttpResponseRedirect(reverse("home"))
+        
         viewData = {}
-        product = Product.products[int(id) - 1]
-        viewData["title"] = product["name"] + " - Product information"
+        product = get_object_or_404(Product, pk=product_id)
+        viewData["title"] = product.name + " - Online Store"
+        viewData["subtitle"] = product.name + " - Producdt information"
         viewData["product"] = product
         
         return render(request, self.template_view, viewData)
     
-class ProductForm(forms.Form):
-    name = forms.CharField(required=True)
-    price = forms.FloatField(required=True)
+class ProductListView(ListView):
+    model = Product
+    template_name = 'product_list.html'
+    context_object_name = 'products'  # This will allow you to loop through 'products' in your template
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Products - Online Store'
+        context['subtitle'] = 'List of products'
+        return context   
+
+class ProductForm(forms.ModelForm):
     
+    class Meta:
+        model = Product
+        fields = ['name', 'price']
+
     def clean_price(self):
-        price = self.cleaned_data["price"]
-        if price <= 0:
-            raise forms.ValidationError("El precio debe ser mayor a cero")
+        price = self.cleaned_data.get('price')
+        if price is not None and price <= 0:
+            raise ValueError('Price must be greater than zero.')
         return price
+
     
 class ProductCreateView(View):
     template_name = 'products/create.html'
@@ -74,7 +96,10 @@ class ProductCreateView(View):
     def post(self, request):
         form = ProductForm(request.POST)
         if form.is_valid():
-            return render(request, "products/product_created.html")
+            form.save()
+            viewData = {}
+            viewData["title"] = "Product created"
+            return render(request, "products/product_created.html", viewData)
         else:
             viewData = {}
             viewData["title"] = "Create product"
@@ -119,3 +144,32 @@ class CartRemoveAllView(View):
             del request.session["cart_product_data"]
             
         return redirect("cart_index")
+    
+def ImageViewFactory(image_storage):
+    class ImageView(View):
+        template_name = "images/index.html"
+        
+        def get(self, request):
+            image_url = request.session.get('image_url', '')
+            return render(request, self.template_name, {'image_url': image_url})
+
+        def post(self, request):
+            image_url = image_storage.store(request)
+            request.session['image_url'] = image_url
+            return redirect('image_index')
+    return ImageView
+
+class ImageViewNoDI(View):
+    template_name = 'images/index.html'
+
+    def get(self, request):
+        image_url = request.session.get('image_url', '')
+        
+        return render(request, self.template_name, {'image_url': image_url})
+
+    def post(self, request):
+        image_storage = ImageLocalStorage()
+        image_url = image_storage.store(request)
+        request.session['image_url'] = image_url
+
+        return redirect('image_index')
